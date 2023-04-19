880c57f7f50f8681c0e0885003ae2b7e8eeb430c
elejeune11 <52667645+elejeune11@users.noreply.github.com>
2023-04-19 19:13:12 -0400

    updating tutorial

--- tutorial/tutorial_example.py
+++ tutorial/tutorial_example.py
@@ -28,14 +28,14 @@ geom_mat, xx_sensor, yy_sensor, height = mg.read_design_grid(mypath, num_grid_ro
 mg.create_mesh_design_grid(mypath, fname, mesh_param, geom_mat, row_dim, col_dim)
 
 # 4. run FEA simulations -- note this step is default set to False as it is better implemented on a workstation -- we provide example results saved as a text file
+step_size = 0.001
+num_steps = 11
 if False:
     fix_whole_btm = True
     mypath_res = Path(__file__).resolve().parent.joinpath("FEA_results").resolve()
     mesh_param = 200
     fname_mesh = fname + "_mesh_%i" % (mesh_param)
     save_pvd = False
-    step_size = 0.001
-    num_steps = 11
     _, xx_sensor_bc_list, yy_sensor_bc_list, height = mg.read_design_grid(mypath, 25, 25, fname, 10, 10)
     for applied_load_type in range(1, 21):
         for applied_load_num in range(1, 21):
@@ -54,17 +54,29 @@ if False:
 data_path = Path(__file__).resolve().parent.joinpath("data").resolve()
 data = np.loadtxt(str(data_path) + "/" + fname + ".txt")
 
+num_pts = 1000
+all_loads = np.load("data/all_loads.npy")
+
+
+def area_under_curve(y_load, num_pts):
+    x_load = np.linspace(0, 10, num_pts)
+    area = np.trapz(y_load, x_load)
+    return -1.0 * area * step_size * (num_steps - 1)
+
+
+area_under_curve_list = []
+for kk in range(0, len(all_loads)):
+    for jj in range(0, all_loads[kk].shape[0]):
+        # all_loads[kk][jj, :] = all_loads[kk][jj, :] / np.sqrt(np.sum(all_loads[kk][jj, :] ** 2.0))
+        area_of_load = area_under_curve(all_loads[kk][jj, :], num_pts)
+        all_loads[kk][jj, :] = all_loads[kk][jj, :] / area_of_load
+        area_under_curve_list.append(area_of_load)
+
 
 def normalize_rows(arr):
     for kk in range(arr.shape[0]):
-        arr[kk, :] = arr[kk, :] / np.sum(arr[kk, :])
+        arr[kk, :] = arr[kk, :] / area_under_curve_list[kk]
     return arr
-
-
-def normalize_rows_list(arr_list):
-    for kk in range(0, len(arr_list)):
-        arr_list[kk] = normalize_rows(arr_list[kk])
-    return arr_list
 
 
 # 6. assess performance of the MechHS systems
